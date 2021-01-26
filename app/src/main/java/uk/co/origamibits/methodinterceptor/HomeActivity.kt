55d22cfc426ef12e528c64e788e549cb7d1d87ac
Christian Garcia <christian.garcia@origamibits.co.uk>
2021-01-26 10:34:01 +0000

    Rename

--- app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
+++ app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
@@ -7,7 +7,7 @@ import timber.log.Timber
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
 
     fun buttonClicked(view: View) {
-        val result = executeTransaction { transaction, context ->
+        val result = executeInTransaction { transaction, context ->
             // Method code start
             (0..100000).map {
                 Item("${transaction.id} - $it")
@@ -18,7 +18,7 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
     }
 
     fun anotherMethod() {
-        executeTransaction { transaction, interceptContext ->
+        executeInTransaction { transaction, interceptContext ->
             // Actual method code
         }
     }
