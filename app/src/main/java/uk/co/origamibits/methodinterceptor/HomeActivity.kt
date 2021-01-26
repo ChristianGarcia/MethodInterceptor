b7bb430f7ea8e108e9445868827b2a229ed1833d
Christian Garcia <christian.garcia@origamibits.co.uk>
2021-01-26 10:30:53 +0000

    Use complex data

--- app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
+++ app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
@@ -3,22 +3,20 @@ package uk.co.origamibits.methodinterceptor
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
-import java.util.*
-import kotlin.system.measureTimeMillis
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
 
     fun buttonClicked(view: View) {
-        val result = intercept { transactionId ->
+        val result = intercept { transaction, context ->
             (0..100000).map {
-                Item("$transactionId - $it")
+                Item("${transaction.id} - $it")
             }
         }
         Timber.d("List size is %s", result.size)
     }
 
     fun anotherMethod() {
-        intercept {
+        intercept { transaction, interceptContext ->
             // Actual method code
         }
     }
@@ -26,14 +24,3 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
 }
 
 data class Item(val id: String)
-
-private fun <T> intercept(block: (String) -> T): T {
-    Timber.d("Before. Measuring method")
-    val transactionId = UUID.randomUUID().toString()
-    var result: T?
-    val executionTime = measureTimeMillis {
-        result = block(transactionId)
-    }
-    Timber.d("After. Time spent %s ms", executionTime)
-    return result!!
-}
---
+++ app/src/main/java/uk/co/origamibits/methodinterceptor/Intercept.kt
@@ -0,0 +1,32 @@
+package uk.co.origamibits.methodinterceptor
+
+import timber.log.Timber
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun <T> intercept(block: (Transaction, InterceptContext) -> T): T {
+    Timber.d("Before. Measuring method")
+    val transaction = Transaction(
+        id = UUID.randomUUID().toString()
+    )
+    val context = InterceptContext(
+        data = mapOf(
+            "key0" to "value0",
+            "key2" to "value1"
+        )
+    )
+    var result: T?
+    val executionTime = measureTimeMillis {
+        result = block(transaction, context)
+    }
+    Timber.d("After. Time spent %s ms", executionTime)
+    return result!!
+}
+
+data class Transaction(
+    val id: String
+)
+
+data class InterceptContext(
+    val data: Map<String, Any>
+)
