5128c962e98de8159fa8fb2cbf8fc71fe0a9560a
Christian Garcia <christian.garcia@origamibits.co.uk>
2021-01-26 10:22:29 +0000

    Add params and result to execution block

--- app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
+++ app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
@@ -3,16 +3,18 @@ package uk.co.origamibits.methodinterceptor
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
+import java.util.*
 import kotlin.system.measureTimeMillis
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
 
     fun buttonClicked(view: View) {
-        intercept {
-            (0..1000000).map {
-                Item("$it")
+        val result = intercept { transactionId ->
+            (0..100000).map {
+                Item("$transactionId - $it")
             }
         }
+        Timber.d("List size is %s", result.size)
     }
 
     fun anotherMethod() {
@@ -21,14 +23,17 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
         }
     }
 
-    private fun intercept(block: () -> Unit) {
-        Timber.d("Before. Measuring method")
-        val executionTime = measureTimeMillis {
-            block()
-        }
-        Timber.d("After. Time spent %s ms", executionTime)
-    }
-
 }
 
 data class Item(val id: String)
+
+private fun <T> intercept(block: (String) -> T): T {
+    Timber.d("Before. Measuring method")
+    val transactionId = UUID.randomUUID().toString()
+    var result: T?
+    val executionTime = measureTimeMillis {
+        result = block(transactionId)
+    }
+    Timber.d("After. Time spent %s ms", executionTime)
+    return result!!
+}
