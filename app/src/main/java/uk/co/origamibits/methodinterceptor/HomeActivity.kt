2f838a92472103fcb48d6fb3fcace7a1bc7eda88
Christian Garcia <christian.garcia@origamibits.co.uk>
2021-01-26 10:32:38 +0000

    Comments

--- app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
+++ app/src/main/java/uk/co/origamibits/methodinterceptor/HomeActivity.kt
@@ -8,9 +8,11 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
 
     fun buttonClicked(view: View) {
         val result = intercept { transaction, context ->
+            // Method code start
             (0..100000).map {
                 Item("${transaction.id} - $it")
             }
+            // Method code end
         }
         Timber.d("List size is %s", result.size)
     }
